dabf471fc569f441d1db4170ce337b67230853d6
nicolas <nicolas@marianes-laptop.(none)>
2010-05-07 18:52:01 -0300

    add flexibility for post/publish methods

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,9 @@
 class SearchController < ApplicationController
   
-  def show
+  def index
+  end
+  
+  def create
     result = client(session[:access_token]).search.query(params[:q]).on(params[:on]).info
     render :json => result
   end
--- app/controllers/selections_controller.rb
+++ app/controllers/selections_controller.rb
@@ -1,2 +1,13 @@
 class SelectionsController < ApplicationController
+  
+  def index
+  end
+  
+  def create
+    selection = client(session[:access_token]).selection
+    selection.send(params[:type], params[:object])
+    selection.send(params[:connection_type]) unless params[:connection_type].blank
+    render :json => selection.info
+  end
+
 end
